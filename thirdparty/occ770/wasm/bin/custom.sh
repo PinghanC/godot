5bf569431c16ca99deead395b55390e3e17fd7cd
djxia <duxia.fr@gmail.com>
2023-12-17 10:52:27 +0100

    add occ770 dlls

---
+++ thirdparty/occ770/wasm/bin/custom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+if [ -e "${aScriptPath}/custom_clang_32.sh" ]; then
+  source "${aScriptPath}/custom_clang_32.sh" "$1" "$2" 
+fi
+
+ 
+if [ -e "${aScriptPath}/custom_clang_32.sh" ]; then
+  source "${aScriptPath}/custom_clang_32.sh" "$1" "$2" 
+fi 
+if [ -e "${aScriptPath}/custom_clang_32.sh" ]; then
+  source "${aScriptPath}/custom_clang_32.sh" "$1" "$2" 
+fi
